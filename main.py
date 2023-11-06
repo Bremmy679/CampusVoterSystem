0d0841786e557313605ec4197886f719fafc705a
Vee2002 <kerubomosomi7@gmail.com>
2023-11-06 11:07:50 +0300

    Changes in main

--- main.py
+++ main.py
@@ -42,6 +42,12 @@ def sendMail():
     mail.send(msg)
     return "Sent"
 
+# Select Page
+# @app.route('/Select Page',methods=['GET', 'POST'])
+# def select_page():
+    
+
+    
 # The user registration
 @app.route('/Register Student', methods=['GET', 'POST'])
 def register():
@@ -164,7 +170,7 @@ def login():
 @app.route("/dashboard")
 def dashboard():
     positions = get_posts()
-    Positions = [position['name'].upper() for position in positions]
+    Positions = [position['name'].upper() for position in positions]            
     return render_template('dashboard.html',Positions=Positions,positions=positions)
 
 @app.route("/Register Admin")
